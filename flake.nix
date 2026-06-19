36ccaba13845cf28573c90a623552218d3e32434
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2026-06-19 22:53:57 +0900

    setup flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A development environment for the guidelines project.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+        };
+      });
+}

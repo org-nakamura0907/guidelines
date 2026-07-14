8cbdd636a98a0aacba2c50bb2cc00bd557df5a40
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2026-07-14 23:31:03 +0900

    CDパイプラインの作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://org-nakamura0907.github.io",
+    base: "/guidelines",
+});

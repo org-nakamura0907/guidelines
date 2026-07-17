6f82a2824c9ac66235387cd14eaa8e940cd93b09
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2026-07-17 17:45:57 +0900

    tailwindの導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import { APP_BASE_PATH, APP_BASE_URL } from './src/shared/config';
 import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
     site: APP_BASE_URL,
     base: APP_BASE_PATH,
     integrations: [mdx()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
